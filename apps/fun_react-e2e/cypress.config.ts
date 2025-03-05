92968371f79d43908bfb5d12242b8f246ef736c8
Anton Shabatin <a.shabatin@gmail.com>
2025-03-05 21:49:39 +0100

    Add React remote app

---
+++ apps/fun_react-e2e/cypress.config.ts
@@ -0,0 +1,16 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'nx run fun_react:serve',
+        production: 'nx run fun_react:serve:production',
+      },
+      ciWebServerCommand: 'nx run fun_react:serve-static',
+    }),
+    baseUrl: 'http://localhost:4202',
+  },
+});
