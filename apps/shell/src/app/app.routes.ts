1e11ee115b61aacc738167af3dd2f550ef7cd944
Anton Shabatin <a.shabatin@gmail.com>
2025-03-05 19:31:07 +0100

    Add remote angular app "fun_ng"

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'angular',
+    loadChildren: () => import('fun_ng/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: '',
     component: NxWelcomeComponent,
   },
